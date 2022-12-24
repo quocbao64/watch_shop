5ae4ef14c52cf6315e17e5a3d45e3645491ecff1
quocbao64 <quocbao642002@gmail.com>
2022-12-24 14:14:46 +0700

    add bar chart in admin dashboard

--- js/admin.js
+++ js/admin.js
@@ -2,21 +2,8 @@ let classes = (classes) => document.getElementsByClassName(classes);
 
 const formatter = new Intl.NumberFormat("en-US");
 
-const getListUsers = async () => {
-    const response = await fetch("http://localhost:8080/api/account/users", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${
-                JSON.parse(localStorage.getItem("user")).accessToken
-            }`,
-        },
-    });
-    return response.json();
-};
-
-const getStatisticOrder = async () => {
-    const response = await fetch("http://localhost:8080/api/statistic/order", {
+const fetchApi = async (url) => {
+    const response = await fetch(url, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -29,11 +16,19 @@ const getStatisticOrder = async () => {
 };
 
 const render = async () => {
-    const users = await getListUsers();
-    const statisticOrder = await getStatisticOrder();
+    const users = await fetchApi("http://localhost:8080/api/account/users");
+    const statisticOrder = await fetchApi(
+        "http://localhost:8080/api/statistic/order"
+    );
+    const listCategory = await fetchApi("http://localhost:8080/api/category");
+    const statisticProduct = await fetchApi(
+        "http://localhost:8080/api/statistic/product"
+    );
 
     console.log("USERS: ", users);
     console.log("ORDERS: ", statisticOrder);
+    console.log("CATEGORY: ", listCategory);
+    console.log("PRODUCT: ", statisticProduct);
 
     let textOrders = classes("order-counter")[0];
     textOrders.innerHTML = statisticOrder?.listOrderSuccess.length;
@@ -57,6 +52,68 @@ const render = async () => {
 
     let textUsers = classes("user-counter")[0];
     textUsers.innerHTML = users?.length;
+
+    var ctx1 = document.getElementById("lineChart").getContext("2d");
+
+    let dataNotSold = statisticProduct?.listProductNotSold || [];
+    let dataSold = statisticProduct?.listProductSold || [];
+    let nameCategories = listCategory?.map((e) => e.name);
+
+    let dataInStockChart = nameCategories?.map((e) => {
+        let category = dataNotSold.find((a) => a.category.name == e);
+        if (category) {
+            return category.quantity;
+        } else {
+            return 0;
+        }
+    });
+
+    let dataSoldChart = nameCategories?.map((e) => {
+        let category = dataSold.find((a) => a.category.name == e);
+        if (category) {
+            return category.quantity;
+        } else {
+            return 0;
+        }
+    });
+
+    var lineChart = new Chart(ctx1, {
+        type: "bar",
+        data: {
+            labels: nameCategories,
+            datasets: [
+                {
+                    label: "In stock",
+                    backgroundColor: "#3EB9DC",
+                    data: dataInStockChart || [],
+                },
+                {
+                    label: "Product sold",
+                    backgroundColor: "#EBEFF3",
+                    data: dataSoldChart || [],
+                },
+            ],
+        },
+        options: {
+            tooltips: {
+                mode: "index",
+                intersect: false,
+            },
+            responsive: true,
+            scales: {
+                xAxes: [
+                    {
+                        stacked: true,
+                    },
+                ],
+                yAxes: [
+                    {
+                        stacked: true,
+                    },
+                ],
+            },
+        },
+    });
 };
 
 render();
